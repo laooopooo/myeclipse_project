5f6f6a7f53c44ac1f19f096ed2483d3cf203d6dc
pfjia <2431631881@qq.com>
2017-03-10 15:49:01 +0800

    再次删除一些文件

--- 构造函数/src/构造函数/Main.java
+++
@@ -1,9 +0,0 @@
-package ¹¹Ôìº¯Êı;
-
-public class Main {
-	public static void main(String[] args) {
-		Chinese c = new Chinese();
-		System.out.println("-----------------");
-		c = new Chinese(7, "");
-	}
-}
--- 构造函数/src/构造函数/Person.java
+++
@@ -1,9 +0,0 @@
-package 构造函数;
-
-public class Person {
-	public Person() {
-		// TODO Auto-generated constructor stub
-		System.out.println("Person的无参构造函数");
-	}
-
-}
