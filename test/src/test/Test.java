6bf2f388d2248ad2e4186f8a8ac4ad2ffa9aaa31
pfjia <2431631881@qq.com>
2017-04-19 22:31:36 +0800

    Revert "1.上午看并发"
    This reverts commit b77a1aef81176fcf9cd3062fd2592bf0d23fe9f8.

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -1,16 +1,22 @@
 package test;
 
-public class Test {
-	static void f() {
-		try {
-			System.out.println("try×Ó¾ä");
-			return;
-		} finally {
-			System.out.println("finally×Ó¾ä");
-		}
+class MyThread extends Thread {
+	public static volatile int i = 0;
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		i++;
 	}
+}
+
+public class Test {
 
 	public static void main(String[] args) {
-		f();
+		for (int i = 0; i < 1000; i++) {
+			new MyThread().start();
+		}
+		System.out.println(MyThread.i);
 	}
+
 }
